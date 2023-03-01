b77a274b9639926085d366c7f7f12c66820c85a1
Ballsky22 <ballskyteam22@gmail.com>
2023-03-01 22:56:32 +0700

    Refactoring Sederhana

---
+++ refactoring/script.js
@@ -0,0 +1,18 @@
+// var a = 8;
+// var b = 3;
+// var volumeA;
+// var volumeB;
+// var total;
+
+// volumeA = a * a * a;
+// volumeB = b * b * b;
+
+// total = volumeA + volumeB;
+
+// console.log(total);
+
+function jumlahVolumeDuaKubus(a, b) {
+  return a * a * a + b * b * b;
+}
+
+alert(jumlahVolumeDuaKubus(8, 3));
