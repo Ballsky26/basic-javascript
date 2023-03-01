4280b051c9059e923b910bc6f83e9f683db4175b
Ballsky22 <ballskyteam22@gmail.com>
2023-03-01 22:23:37 +0700

    Membuat Function

---
+++ function/script.js
@@ -0,0 +1,28 @@
+// var a = 8;
+// var b = 3;
+// var volumeA;
+// var volumeB;
+// var total;
+
+// volumeA = a * a * a;
+// volumeB = b * b * b;
+
+// total = volumeA + volumeB;
+
+// console.log(total);
+
+function jumlahVolumeDuaKubus(a, b) {
+  var volumeA;
+  var volumeB;
+  var total;
+
+  volumeA = a * a * a;
+  volumeB = b * b * b;
+
+  total = volumeA + volumeB;
+
+  return total;
+}
+
+console.log("Total Volume", jumlahVolumeDuaKubus(8, 3));
+console.log("Total Volume", jumlahVolumeDuaKubus(10, 15));
