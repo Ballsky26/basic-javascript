3e1caaa89f8957ca6b31e4070300adffa2f85170
Ballsky22 <ballskyteam22@gmail.com>
2023-03-06 20:56:14 +0700

    Array Multidimensi/Bersarang

--- array/script.js
+++ array/script.js
@@ -3,4 +3,11 @@ var binatang = ["Kucing", "Kelinci", "Monyet", "Panda", "Koala", "Sapi"];
 console.log(binatang[4]);
 
 // Bertipe Object
-console.log(typeof binatang);
+// console.log(typeof binatang);
+
+// Memiliki method Length
+// console.log(binatang.length);
+
+// Array Multidimensi
+var myArray = ["teks", 2, false, myfunction, [4, 5, 6]];
+console.log(myArray[4][1]);
