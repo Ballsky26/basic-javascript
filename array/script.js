105d3189593be54783db850564fa99bd389b8b34
Ballsky22 <ballskyteam22@gmail.com>
2023-03-06 20:51:03 +0700

    Array Bertipe Object

--- array/script.js
+++ array/script.js
@@ -1,3 +1,6 @@
 // Key and value Pair - Mengakses elemen Pada Array
 var binatang = ["Kucing", "Kelinci", "Monyet", "Panda", "Koala", "Sapi"];
 console.log(binatang[4]);
+
+// Bertipe Object
+console.log(typeof binatang);
