dc4692f601e18f9786004bb178ae313e018298e4
Ballsky22 <ballskyteam22@gmail.com>
2023-03-07 19:59:42 +0700

    Object Javascript

---
+++ object/script.js
@@ -0,0 +1,11 @@
+// Membuat Object
+var mahasiswa = {
+  nama: "Syaiful Iqbal",
+  umur: 23,
+  ips: [3.0, 3.02, 3.69],
+  alamat: {
+    jalan: "Jl. Street Mobile",
+    kota: "Midland",
+    provinsi: "Java",
+  },
+};
