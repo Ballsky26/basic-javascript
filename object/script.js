71a328734e9b05d28e748250af78709523cc03f9
Ballsky22 <ballskyteam22@gmail.com>
2023-03-07 20:05:35 +0700

    Object Literal Javascript

--- object/script.js
+++ object/script.js
@@ -1,11 +1,25 @@
 // Membuat Object
 // Literal
-var mahasiswa = {
+var mahasiswa1 = {
   nama: "Syaiful Iqbal",
   nrp: "182400015",
   email: "Ballskyteam22@gmail.com",
   jurusan: "Teknik Informatika",
   umur: 23,
+  ips: [3.0, 3.02, 3.69],
+  alamat: {
+    jalan: "Jl. Street Mobile",
+    kota: "Midland",
+    provinsi: "Java",
+  },
+};
+
+var mahasiswa2 = {
+  nama: "Achmad Fahdil",
+  nrp: "182400077",
+  email: "Achmadfahdil46@gmail.com",
+  jurusan: "Teknik Informatika",
+  umur: 24,
   ips: [3.0, 3.02, 3.69],
   alamat: {
     jalan: "Jl. Street Mobile",
