279730869881f764f8a661e4dd9f00d321e97250
Ballsky22 <ballskyteam22@gmail.com>
2023-03-07 20:04:17 +0700

    Object Literal Javascript

--- object/script.js
+++ object/script.js
@@ -1,6 +1,10 @@
 // Membuat Object
+// Literal
 var mahasiswa = {
   nama: "Syaiful Iqbal",
+  nrp: "182400015",
+  email: "Ballskyteam22@gmail.com",
+  jurusan: "Teknik Informatika",
   umur: 23,
   ips: [3.0, 3.02, 3.69],
   alamat: {
