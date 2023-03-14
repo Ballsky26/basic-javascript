187024c059a37d401e4b0c599f8f839e02a708bf
Ballsky22 <ballskyteam22@gmail.com>
2023-03-14 23:29:09 +0700

    Var, Let, Const

---
+++ var-let-const/script.js
@@ -0,0 +1,45 @@
+// var function scope
+// var i = 10;
+// console.log(i);
+
+// for (var i = 0; i < 10; i++) {
+//     console.log(i);
+//   }
+//   console.log(i);
+
+// function test() {
+//   for (var i = 0; i < 10; i++) {
+//     console.log(i);
+//   }
+// }
+// test();
+
+// (function () {
+//   for (var i = 0; i < 10; i++) {
+//     console.log(i);
+//   }
+// })();
+
+// let Block Scope
+// for (let i = 0; i < 10; i++) {
+//   console.log(i);
+// }
+// console.log(i);
+
+// const = kalo variabel gak pernah ganti
+// const i = 10;
+// i = 55;
+// const mhs = {
+//   nama: "Ballsky",
+//   umur: 23,
+// };
+// mhs.umur = 22;
+// console.log(mhs);
+
+const i = [1, 2, 3];
+i.push(4);
+console.log(i);
+// let = gunakan saat bikin for
+// for (let i = 0; i < 10; i++) {
+//   console.log(i);
+// }
